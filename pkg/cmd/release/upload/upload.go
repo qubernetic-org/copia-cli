8bf168be72079c652f60782c1d8a158831f21458
Csaba Attila Biró <csaba.biro@qubernetic.com>
2026-04-01 14:52:53 +0200

    feat(release): add upload command for release assets

---
+++ pkg/cmd/release/upload/upload.go
@@ -0,0 +1,162 @@
+package upload
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"github.com/qubernetic-org/copia-cli/pkg/cmdutil"
+	"github.com/qubernetic-org/copia-cli/pkg/iostreams"
+)
+
+type UploadOptions struct {
+	IO         *iostreams.IOStreams
+	HTTPClient *http.Client
+	Host       string
+	Token      string
+	Owner      string
+	Repo       string
+	Tag        string
+	Files      []string
+}
+
+func NewCmdUpload(f *cmdutil.Factory) *cobra.Command {
+	opts := &UploadOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "upload <tag> <files>...",
+		Short: "Upload assets to a release",
+		Example: `  copia release upload v1.0.0 binary.tar.gz
+  copia release upload v1.0.0 *.tar.gz *.zip`,
+		Args: cobra.MinimumNArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Tag = args[0]
+			opts.Files = args[1:]
+			opts.IO = f.IOStreams
+
+			host, token, err := f.ResolveAuth()
+			if err != nil {
+				return err
+			}
+			opts.Host = host
+			opts.Token = token
+
+			if f.BaseRepo == nil {
+				return fmt.Errorf("could not determine repository. Use --repo flag")
+			}
+			owner, repo, err := f.BaseRepo()
+			if err != nil {
+				return err
+			}
+			opts.Owner = owner
+			opts.Repo = repo
+			opts.HTTPClient = &http.Client{}
+			return uploadRun(opts)
+		},
+	}
+
+	return cmd
+}
+
+func uploadRun(opts *UploadOptions) error {
+	// Validate files exist first
+	for _, f := range opts.Files {
+		if _, err := os.Stat(f); err != nil {
+			return fmt.Errorf("opening %s: %w", f, err)
+		}
+	}
+
+	// Look up release ID by tag
+	lookupURL := fmt.Sprintf("https://%s/api/v1/repos/%s/%s/releases/tags/%s",
+		opts.Host, opts.Owner, opts.Repo, opts.Tag)
+
+	lookupReq, err := http.NewRequest("GET", lookupURL, nil)
+	if err != nil {
+		return err
+	}
+	lookupReq.Header.Set("Authorization", "token "+opts.Token)
+
+	lookupResp, err := opts.HTTPClient.Do(lookupReq)
+	if err != nil {
+		return fmt.Errorf("connecting to %s: %w", opts.Host, err)
+	}
+	defer func() { _ = lookupResp.Body.Close() }()
+
+	if lookupResp.StatusCode == http.StatusNotFound {
+		return fmt.Errorf("release %s not found", opts.Tag)
+	}
+
+	lookupBody, err := io.ReadAll(lookupResp.Body)
+	if err != nil {
+		return err
+	}
+
+	var release struct {
+		ID int64 `json:"id"`
+	}
+	if err := json.Unmarshal(lookupBody, &release); err != nil {
+		return err
+	}
+
+	// Upload each file
+	for _, filePath := range opts.Files {
+		if err := uploadFile(opts, release.ID, filePath); err != nil {
+			return fmt.Errorf("uploading %s: %w", filepath.Base(filePath), err)
+		}
+		_, _ = fmt.Fprintf(opts.IO.Out, "Uploaded %s to %s\n", filepath.Base(filePath), opts.Tag)
+	}
+
+	return nil
+}
+
+func uploadFile(opts *UploadOptions, releaseID int64, filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = file.Close() }()
+
+	pr, pw := io.Pipe()
+	writer := multipart.NewWriter(pw)
+
+	go func() {
+		part, err := writer.CreateFormFile("attachment", filepath.Base(filePath))
+		if err != nil {
+			_ = pw.CloseWithError(err)
+			return
+		}
+		if _, err := io.Copy(part, file); err != nil {
+			_ = pw.CloseWithError(err)
+			return
+		}
+		_ = writer.Close()
+		_ = pw.Close()
+	}()
+
+	url := fmt.Sprintf("https://%s/api/v1/repos/%s/%s/releases/%d/assets",
+		opts.Host, opts.Owner, opts.Repo, releaseID)
+
+	req, err := http.NewRequest("POST", url, pr)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "token "+opts.Token)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	resp, err := opts.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+	_ = resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("HTTP %d", resp.StatusCode)
+	}
+
+	return nil
+}
