7ea862bd8350f4185253822a26619114f13465f8
Csaba Attila Biró <csaba.biro@qubernetic.com>
2026-04-01 17:05:28 +0200

    feat(pr): add diff command to view PR changes

---
+++ pkg/cmd/pr/diff/diff.go
@@ -0,0 +1,89 @@
+package diff
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/qubernetic-org/copia-cli/pkg/cmdutil"
+	"github.com/qubernetic-org/copia-cli/pkg/iostreams"
+)
+
+type DiffOptions struct {
+	IO         *iostreams.IOStreams
+	HTTPClient *http.Client
+	Host       string
+	Token      string
+	Owner      string
+	Repo       string
+	Number     int64
+}
+
+func NewCmdDiff(f *cmdutil.Factory) *cobra.Command {
+	opts := &DiffOptions{}
+
+	cmd := &cobra.Command{
+		Use:     "diff <number>",
+		Short:   "View diff for a pull request",
+		Example: "  copia pr diff 7",
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			num, err := strconv.ParseInt(args[0], 10, 64)
+			if err != nil {
+				return fmt.Errorf("invalid PR number: %s", args[0])
+			}
+			opts.Number = num
+			opts.IO = f.IOStreams
+
+			host, token, err := f.ResolveAuth()
+			if err != nil {
+				return err
+			}
+			opts.Host = host
+			opts.Token = token
+
+			if f.BaseRepo == nil {
+				return fmt.Errorf("could not determine repository. Use --repo flag")
+			}
+			owner, repo, err := f.BaseRepo()
+			if err != nil {
+				return err
+			}
+			opts.Owner = owner
+			opts.Repo = repo
+			opts.HTTPClient = &http.Client{}
+			return diffRun(opts)
+		},
+	}
+
+	return cmd
+}
+
+func diffRun(opts *DiffOptions) error {
+	url := fmt.Sprintf("https://%s/api/v1/repos/%s/%s/pulls/%d.diff",
+		opts.Host, opts.Owner, opts.Repo, opts.Number)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "token "+opts.Token)
+
+	resp, err := opts.HTTPClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("connecting to %s: %w", opts.Host, err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode == http.StatusNotFound {
+		return fmt.Errorf("PR #%d not found", opts.Number)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("API error (HTTP %d)", resp.StatusCode)
+	}
+
+	_, err = io.Copy(opts.IO.Out, resp.Body)
+	return err
+}
