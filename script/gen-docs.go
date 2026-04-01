86de6b3427c0d0fc73acd3c6185f566dd5255345
Csaba Attila Biró <csaba.biro@qubernetic.com>
2026-04-01 23:35:27 +0200

    feat: add Cobra doc generation script for command reference

---
+++ script/gen-docs.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra/doc"
+	"github.com/qubernetic-org/copia-cli/internal/config"
+	"github.com/qubernetic-org/copia-cli/internal/copiacmd"
+	"github.com/qubernetic-org/copia-cli/pkg/cmdutil"
+	"github.com/qubernetic-org/copia-cli/pkg/iostreams"
+)
+
+func main() {
+	outDir := "docs/manual/src/commands"
+	if len(os.Args) > 1 {
+		outDir = os.Args[1]
+	}
+
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating output dir: %v\n", err)
+		os.Exit(1)
+	}
+
+	ios := iostreams.System()
+	f := &cmdutil.Factory{
+		IOStreams: ios,
+		Config: func() (*config.Config, error) {
+			return &config.Config{Hosts: map[string]*config.HostConfig{}}, nil
+		},
+	}
+
+	rootCmd := copiacmd.NewRootCmd(f)
+	rootCmd.DisableAutoGenTag = true
+
+	linkHandler := func(name string) string {
+		base := strings.TrimSuffix(name, ".md")
+		return base + ".md"
+	}
+
+	filePrepender := func(filename string) string {
+		name := filepath.Base(filename)
+		name = strings.TrimSuffix(name, ".md")
+		name = strings.ReplaceAll(name, "_", " ")
+		return fmt.Sprintf("# %s\n\n", name)
+	}
+
+	if err := doc.GenMarkdownTreeCustom(rootCmd, outDir, filePrepender, linkHandler); err != nil {
+		fmt.Fprintf(os.Stderr, "Error generating docs: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Generated command docs in %s\n", outDir)
+}
